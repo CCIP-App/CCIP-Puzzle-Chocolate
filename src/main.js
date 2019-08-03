f2b1b6bb748eedf3782cb713771d71a7b281b07b
Erickson Zhuang <bingluen0224@gmail.com>
2019-08-03 14:18:57 +0800

    Add i18n package

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import Components from './components/_index'
+import i18n from './i18n'
 
 Vue.config.productionTip = false
 
@@ -11,5 +12,6 @@ Object.keys(Components).forEach(key => {
 
 new Vue({
   router,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
