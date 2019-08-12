d53a2612eb93a0faf70d050778cf2211b3a439d4
Erickson Zhuang <bingluen0224@gmail.com>
2019-08-12 15:10:59 +0800

    Integrate reward and API

--- src/views/Reward.vue
+++ src/views/Reward.vue
@@ -1,89 +1,81 @@
 <template>
-  <div>
-    <h1>{{ $t('reward') }}</h1>
-    <h2>{{ $t('has_got_points', {points: gotPoints}) }}</h2>
+  <div role="reward">
+    <h1 role="GameName">{{ $t('reward') }}</h1>
     <template v-if="showScanner">
       <qrcode-reader
             :enable="showScanner"
             :noResult="true"
             title=""
             :subTitle="$t('scan_qrcode')"
-            @OnSuccess="OnSuccess"
+            @success="onScanSuccess"
           ></qrcode-reader>
     </template>
-    <template v-else>
-      <div role="chips">
-        <template v-for="(booth, index) in booths">
-          <Chip :logoUri="booth.image_url" :isActive="userSeal.findIndex(seal => seal.deliver === booth.slug)> -1" :key="index" />
-        </template>
-      </div>
+    <template v-if="playerPubToken !== null">
+      <h2 role="got-points">{{ $t('has_got_points', {points: gotPoints}) }}</h2>
+      <SquareGrid :booths="boothList" :userStamps="stamps" :showAnchor="true" />
+      <BoothList :booths="booths" />
     </template>
   </div>
 </template>
 
 <script>
-import QrcodeReader from '@/components/QrcodeReader.vue'
-import Chip from '@/components/Chip.vue'
-
+import { mapGetters } from 'vuex'
 export default {
   name: 'Reward',
-  component: {
-    Chip,
-    QrcodeReader
-  },
-  data () {
-    return {
-      booths: [
-        {
-          significant: 'c',
-          slug: 'opass',
-          display_text: {
-            'zh-TW': 'OPass',
-            en: 'OPass'
-          },
-          image_url: 'https://avatars0.githubusercontent.com/u/24525301?s=200&v=4',
-          point: 10
-        }
-      ],
-      showScanner: false,
-      token: null,
-      userSeal: [
-        {
-          deliver: 'opass',
-          timestamp: 1563645742.220855
-        }
-      ]
-    }
-  },
   computed: {
-    boothList: () => (this.booths.map(booth => ({
-      significant: booth.significant,
-      slug: booth.slug,
-      display_text: booth.display_text[this.$i18n.locale],
-      image_url: booth.image_url,
-      point: booth.point
-    }))),
+    ...mapGetters(['booths', 'stamps', 'playerPubToken']),
+    showScanner () {
+      return this.playerPubToken === null
+    },
+    boothList () {
+      let booths = this.booths.map(booth => ({
+        isBonus: booth.isBonus,
+        significant: booth.significant,
+        slug: booth.slug,
+        displayText: booth.displayText[this.$i18n.locale],
+        imageUrl: booth.imageUrl,
+        point: booth.point
+      }))
+
+      let edge = Math.ceil(Math.sqrt(booths.length, 2))
+      let spaceNum = edge - booths.length % edge || 0
+
+      return booths.concat(Array(spaceNum).fill({
+        isBonus: false,
+        significant: null,
+        slug: null,
+        displayText: null,
+        imageUrl: null,
+        point: 0
+      }))
+    },
     gotPoints () {
-      return this.userSeal.reduce((gotPoints, seal) => {
-        const deliver = this.booths.find((booth) => booth.slug === seal.deliver)
-        return deliver && deliver.point ? gotPoints + deliver.point : gotPoints
+      return this.booths
+        .filter(booth => booth.isBonus)
+        .reduce((gotPoint, booth) => gotPoint + booth.point, 0) + this.stamps.reduce((gotPoints, stamp) => {
+        const deliverer = this.booths.find((booth) => booth.slug === stamp.deliverer && booth.isBonus === false)
+        return deliverer && deliverer.point ? gotPoints + deliverer.point : gotPoints
       }, 0)
     }
   },
-  mounted () {
-    this.loadBoothList()
+  created () {
+    this.$store.dispatch('fetchBooths')
+    const token = this.$route.query.token || null
+    if (token !== null) {
+      this.$store.dispatch('setPubTokenFromToken', token)
+      this.$store.dispatch('fetchPuzzleBook')
+    }
   },
   methods: {
-    loadBoothList () {
-
-    },
-    loadUserContext () {
-
+    onScanSuccess (scanValue) {
+      this.$store.dispatch('setPubTokenFromToken', scanValue)
+      this.$store.dispatch('fetchPuzzleBook')
     }
   }
 }
 </script>
 
 <style lang="stylus">
-
+  [role="got-points"]
+    text-align: center
 </style>
