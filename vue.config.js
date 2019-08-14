3e0a250c18e530dffceabfdf7309e438a8392e51
yoyo930021 <yoyo930021@gmail.com>
2019-08-14 14:03:17 +0800

    Add prerender for github page

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,18 @@ module.exports = {
       fallbackLocale: 'en',
       localeDir: 'locales',
       enableInSFC: true
+    },
+    prerenderSpa: {
+      registry: undefined,
+      renderRoutes: [
+        '/Stamp',
+        '/Reward',
+        '/Bingo',
+        '/Fragment'
+      ],
+      useRenderEvent: true,
+      headless: true,
+      onlyProduction: true
     }
   }
 }
