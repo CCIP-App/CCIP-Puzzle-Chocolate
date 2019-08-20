6b2919676ba8ebff7c9bbfa5a2f7d33080f01a2c
Erickson Zhuang <bingluen0224@gmail.com>
2019-08-21 00:58:41 +0800

    Fix lint warning

--- src/main.js
+++ src/main.js
@@ -16,5 +16,5 @@ new Vue({
   store,
   i18n,
   render: h => h(App),
-  mounted: () => document.dispatchEvent(new Event("x-app-rendered")),
+  mounted: () => document.dispatchEvent(new Event('x-app-rendered'))
 }).$mount('#app')
