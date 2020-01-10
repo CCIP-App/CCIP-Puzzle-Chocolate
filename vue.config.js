9d5a7e24facdf1dec2c8bc18b88d2f345cfdad87
Erickson Zhuang <bingluen0224@gmail.com>
2020-01-10 23:51:32 +0800

    Add new qr-code scanner package

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   pluginOptions: {
     i18n: {
@@ -19,5 +21,17 @@ module.exports = {
       headless: true,
       onlyProduction: true
     }
-  }
+  },
+  chainWebpack: (config) => {
+    config
+      .plugin('copy')
+      .tap(args => {
+        args[0].push({
+          from: path.join(__dirname, 'node_modules/qr-scanner/qr-scanner-worker.min.js'),
+          toType: 'dir',
+          to: path.join(__dirname, './dist/js')
+        })
+        return args
+      })
+  },
 }
