d06d6ff253a4608970101f5ba2d465b6d3f63af8
Denny Huang <denny0223@gmail.com>
2020-07-21 23:31:44 +0800

    ESLint allow console.warn

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
     '@vue/standard'
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': ["error", { allow: ["warn"] }],
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
